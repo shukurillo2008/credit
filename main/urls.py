2caca55c04b8badbf3e83f74e3077b96332360dc
shukurillo2008 <shukurilloisroilov98@gmail.com>
2025-05-04 12:02:24 +0500

    something

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns=[
-    path('d33e16dc-936b-47bd-bc8c-174d0ab67430/',Level2),
+    path('d33e16dc-936b-47bd-bc8c-174d0ab67430/',Level1),
     path('hjfigjrf-1694811802065-358e98633040157adcd721c2c8e402b9cb193f1/',Level2)
 
 ]
