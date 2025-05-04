cc5668b03c724f5231508b9337a6e32a2b284f18
shukurillo2008 <shukurilloisroilov98@gmail.com>
2025-05-04 10:15:06 +0500

    sdsd

--- Admin/settings.py
+++ Admin/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static_root'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
